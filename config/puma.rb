50923d57f6909007c67d548e24436fabf9e26495
Yoshikawa Ryota <yoshikawa@rrreeeyyy.com>
2016-10-17 17:36:28 +0900

    Fix listen address

--- config/puma.rb
+++ config/puma.rb
@@ -8,9 +8,6 @@ state_path "/var/run/puma.state"
 
 stdout_redirect "/tmp/puma.stdout.log", "/tmp/puma.stderr.log", true
 
-bind "tcp://0.0.0.0:9292"
-# bind "unix:///var/run/puma.sock"
-
-port 9293
+bind "unix:///var/run/puma.sock"
 
 workers 2
