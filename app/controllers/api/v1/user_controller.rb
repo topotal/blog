9287427557760cc9cfaa8722d198578caa97c6e8
TAKAMURA Narimichi <nari@nari64.com>
2016-12-09 23:47:08 +0900

    Change api path from `/users/register` to `/users`

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UserController < ::Api::V1::BaseController
-      post "/register" do
+      post "/" do
         name, password = parse_json_or_halt(request.body.read).values_at(:name, :password)
         user = User.create(name: name, password: password, password_confirmation: password)
         user.valid? ? [201, { token: tokenize(name) }.to_json] : [400, user.errors.messages.to_json]
