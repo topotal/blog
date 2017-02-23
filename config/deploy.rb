106ac9a00a59a97780ee5d2f6ed71cf7f6351b8c
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-02-24 01:08:48 +0900

    Initial settings for topotal/blog

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "topotal-blog"
+set :repo_url, "git@github.com:topotal/blog.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
